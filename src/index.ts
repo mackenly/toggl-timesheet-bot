d4c8acef284a7d88cbcb99f8d01e501eb9208838
Mackenly Jones <53151058+mackenly@users.noreply.github.com>
2024-06-22 00:40:37 -0400

    Make some things better (like timezones and custom hourly rate)

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ export interface Env {
 	MYBROWSER: BrowserWorker;
 
 	TOGGL_API_TOKEN: string;
-	HOURLY_RATE: string;
 	ONLY_WORKSPACE: string;
 	YOUR_PHONE: string;
 	YOUR_ADDRESS: string;
@@ -38,6 +37,7 @@ export default {
 		let startDate: Date;
 		let endDate: Date;
 		let clientId: number;
+		let hourlyRate: number;
 
 		// if startDate is provided, use it, otherwise default to 7 days ago
 		if (url.searchParams.has('startDate')) {
@@ -62,8 +62,12 @@ export default {
 				);
 			}
 		} else {
-			endDate = new Date();
-			startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+			// the last second of today
+			endDate = new Date(new Date().setUTCHours(23, 59, 59, 999));
+			startDate = new Date(new Date(endDate.getTime() - 13 * 24 * 60 * 60 * 1000).setUTCHours(0, 0, 0, 0));
+			url.searchParams.set('startDate', startDate.toISOString());
+			url.searchParams.set('endDate', endDate.toISOString());
+			return Response.redirect(url.toString(), 302);
 		}
 
 		const allProjects = await getTogglData(env, startDate, endDate);
@@ -85,7 +89,7 @@ export default {
 			});
 		});
 
-		if (url.searchParams.has('clientId')) {
+		if (url.searchParams.has('clientId') && url.searchParams.get('clientId') !== '') {
 			clientId = parseInt(url.searchParams.get('clientId') as string);
 		} else {
 			clientId = allProjects[0].clients[0].client.id;
@@ -97,6 +101,14 @@ export default {
 				<meta http-equiv='X-UA-Compatible' content='IE=edge'>
 				<meta name='viewport' content='width=device-width, initial-scale=1.0'>
 				<title>Select Client</title>
+				<style>
+					body { font-family: Arial, Helvetica, sans-serif; font-size: 12px; max-width: 480px; margin: 20px auto; }
+					h1 { font-size: 35px; margin-bottom: 10px; }
+					form { display: flex; flex-direction: column; }
+					label { margin-bottom: 10px; }
+					select { padding: 10px; margin-bottom: 20px; }
+					button { padding: 10px; background-color: #4CAF50; color: white; cursor: pointer; }
+				</style>
 			</head>
 			<body>
 				<h1>Select a Client</h1>
@@ -120,7 +132,112 @@ export default {
 		const allClientIds = allProjects[0].clients.map((client: any) => client.client.id);
 		console.log(`All Client IDs: ${allClientIds}`);
 		console.log(`Client ID: ${clientId}`);
-		let html = await createHtml(allProjects, me, startDate, endDate, clientId, env);
+
+		if (url.searchParams.has('hourlyRate') && parseFloat(url.searchParams.get('hourlyRate') as string) > 0) {
+			hourlyRate = parseFloat(url.searchParams.get('hourlyRate') as string);
+		} else {
+			const selectRateHtml = `
+			<!DOCTYPE html>
+			<html lang='en'>
+			<head>
+				<meta charset='UTF-8'>
+				<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+				<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+				<title>Enter Hourly Rate</title>
+				<style>
+					body { font-family: Arial, Helvetica, sans-serif; font-size: 12px; max-width: 480px; margin: 20px auto; }
+					h1 { font-size: 35px; margin-bottom: 10px; }
+					form { display: flex; flex-direction: column; }
+					label { margin-bottom: 10px; }
+					input { padding: 10px; margin-bottom: 20px; }
+					button { padding: 10px; background-color: #4CAF50; color: white; cursor: pointer; }
+				</style>
+			</head>
+			<body>
+				<h1>Hourly Rate</h1>
+				<form action='/' method='get'>
+					<label for='hourlyRate'>Hourly Rate:</label>
+					<input type='number' name='hourlyRate' id='hourlyRate' step='0.01' required>
+					<input type='hidden' name='clientId' value='${clientId}'>
+					<input type='hidden' name='startDate' value='${startDate.toISOString()}'>
+					<input type='hidden' name='endDate' value='${endDate.toISOString()}'>
+					<button type='submit'>Submit</button>
+				</form>
+			</body>
+			</html>`;
+			return new Response(selectRateHtml, {
+				status: 200,
+				headers: {
+					'Content-Type': 'text/html',
+				},
+			});
+		}
+
+		// due date (default to 30 days from now but ask for input)
+		let dueDate: string;
+		if ((url.searchParams.has('dueDate') && url.searchParams.get('dueDate') !== '' && new Date(url.searchParams.get('dueDate') as string) > new Date()) || (url.searchParams.has('daysDue') && parseInt(url.searchParams.get('daysDue') as string) > 0)) {
+			if (url.searchParams.has('daysDue') && parseInt(url.searchParams.get('daysDue') as string) > 0) {
+				dueDate = new Date(new Date().getTime() + parseInt(url.searchParams.get('daysDue') as string) * 24 * 60 * 60 * 1000).toDateString();
+			} else {
+				dueDate = url.searchParams.get('dueDate') as string;
+			}
+		} else {
+			const selectDueDateHtml = `
+			<!DOCTYPE html>
+			<html lang='en'>
+			<head>
+				<meta charset='UTF-8'>
+				<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+				<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+				<title>Select Due Date</title>
+				<style>
+					body { font-family: Arial, Helvetica, sans-serif; font-size: 12px; max-width: 480px; margin: 20px auto; }
+					h1 { font-size: 35px; margin-bottom: 10px; }
+					form { display: flex; flex-direction: column; }
+					label { margin-bottom: 10px; }
+					input { padding: 10px; margin-bottom: 20px; }
+					button { padding: 10px; background-color: #4CAF50; color: white; cursor: pointer; }
+				</style>
+			</head>
+			<body>
+				<h1>Select Due Date</h1>
+				<form action='/' method='none' id='dueDateForm'>
+					<label for='dueDate'>Due Date:</label>
+					<input type='date' name='dueDate' id='dueDate' min='${new Date().toISOString().split('T')[0]}'>
+					<label for='daysDue'>Days:</label>
+					<input type='number' name'daysDue' id='daysDue' min='1' value='30'>
+					<input type='hidden' name='clientId' value='${clientId}'>
+					<input type='hidden' name='startDate' value='${startDate.toISOString()}'>
+					<input type='hidden' name='endDate' value='${endDate.toISOString()}'>
+					<input type='hidden' name='hourlyRate' value='${hourlyRate}'>
+					<button type='submit'>Submit</button>
+				</form>
+				<script>
+					document.getElementById('dueDateForm').addEventListener('submit', (e) => {
+						e.preventDefault();
+						const dueDate = document.getElementById('dueDate').value;
+						const daysDue = document.getElementById('daysDue').value;
+						if (dueDate !== '') {
+							window.location.href = \`/?clientId=${clientId}&startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}&hourlyRate=${hourlyRate}&dueDate=\${dueDate.toISOString()}\`;
+							return;
+						}
+						if (daysDue !== '' || (daysDue === '' && dueDate === '')) {
+							window.location.href = \`/?clientId=${clientId}&startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}&hourlyRate=${hourlyRate}&daysDue=\${daysDue}\`;
+							return;
+						}
+					});
+				</script>
+			</body>
+			</html>`;
+			return new Response(selectDueDateHtml, {
+				status: 200,
+				headers: {
+					'Content-Type': 'text/html',
+				},
+			});
+		}
+
+		let html = await createHtml(allProjects, me, startDate, endDate, clientId, hourlyRate, dueDate, env);
 		//let html;
 		/*const browser = await puppeteer.launch(env.MYBROWSER);
 
@@ -182,7 +299,7 @@ async function getTogglData(env: Env, startDate: Date, endDate: Date) {
 	return allProjects;
 }
 
-async function createHtml(allProjects: any, me: any, startDate: Date, endDate: Date, clientId: number, env: Env) {
+async function createHtml(allProjects: any, me: any, startDate: Date, endDate: Date, clientId: number, hourlyRate: number, dueDate: string, env: Env) {
 	const workspace = allProjects[0].workspace;
 	// if a client Id is not present on workspace, throw an error
 	if (allProjects[0].clients.find((client: any) => client.client.id === clientId) === undefined) {
@@ -199,19 +316,22 @@ async function createHtml(allProjects: any, me: any, startDate: Date, endDate: D
 				year: 'numeric',
 				month: 'numeric',
 				day: 'numeric',
+				timeZone: 'America/New_York',
 			})}</td>
 				<td>${project.project.name} - ${entry.description}</td>
 				<td>${new Date(entry.start).toLocaleTimeString('en-US', {
 				hour: '2-digit',
 				minute: '2-digit',
+				timeZone: 'America/New_York',
 			})}</td>
 				<td>${new Date(entry.stop).toLocaleTimeString('en-US', {
 				hour: '2-digit',
 				minute: '2-digit',
+				timeZone: 'America/New_York',
 			})}</td>
 				<td>${(entry.duration / 3600).toFixed(2)}</td>
-				<td>$${parseFloat(env.HOURLY_RATE).toFixed(2)}</td>
-				<td>$${((entry.duration / 3600) * parseFloat(env.HOURLY_RATE)).toFixed(2)}</td>
+				<td>$${hourlyRate.toFixed(2)}</td>
+				<td>$${((entry.duration / 3600) * hourlyRate).toFixed(2)}</td>
 			</tr>`;
 		});
 	});
@@ -229,7 +349,7 @@ async function createHtml(allProjects: any, me: any, startDate: Date, endDate: D
 			<p>
 				<b>Hourly Rate: </b> 
 				<span>
-					$${parseFloat(env.HOURLY_RATE).toFixed(2)}
+					$${hourlyRate.toFixed(2)}
 				</span>
 			</p>
 			<p>
@@ -239,8 +359,14 @@ async function createHtml(allProjects: any, me: any, startDate: Date, endDate: D
 			return a + b.entries.reduce((c: any, d: any) => c + d.duration, 0);
 		}, 0) /
 			3600) *
-			parseFloat(env.HOURLY_RATE)).toFixed(2)
+			hourlyRate).toFixed(2)
 		}
+				</span>
+			</p>
+			<p>
+				<b>Due Date: </b>
+				<span>
+					${dueDate}
 				</span>
 			</p>
 		</section>`;
@@ -256,14 +382,14 @@ th { padding-top: 12px; padding-bottom: 12px; text-align: left; background-color
 			year: 'numeric',
 			month: 'long',
 			day: 'numeric',
-		})}</span>  </p>  <p>  <b>Invoice Period: </b> <span>${startDate.getMonth() + 1}/${startDate.getDate()}/
-${startDate.getFullYear()} - ${endDate.getMonth() + 1
+			timeZone: 'America/New_York',
+		})}</span>  </p>  <p>  <b>Invoice Period: </b> <span>${startDate.getMonth() + 1}/${startDate.getDate()}/${startDate.getFullYear()} - ${endDate.getMonth() + 1
 		}/${endDate.getDate()}/${endDate.getFullYear()}</span>  </p> </div> <div>  <p>  <b>Contractor: </b> <span>${workspace.name
 		}</span>  </p>  <p>  <b>Email: </b> <span>${me.email}</span>  </p>  <p>  <b> Phone: </b> <span>${env.YOUR_PHONE
 		}</span>  </p>  <p>  <b>Address: </b> <span>${env.YOUR_ADDRESS}</span>  </p> </div> </div> </header><section> 
-<table><tr> <th>Date</th> <th>Project/Task</th> <th>Start (UTC)</th> <th>Stop (UTC)</th> <th>Hours</th> <th>Rate</th> <th>Total</th> </tr>${tableRowsContent}</table></section>${totalsContent}<section> <p> <b>Notice: </b>This invoice is dynamically generated from Toggl data and may be incomplete. The hour totals listed are rounded from the actual time tracked (in milliseconds). The total hours may be slightly off as a result. However, the amount due is calculated 
-off the seconds recorded. Please contact ${me.fullname
-		} if you have any questions or require clarification on any item. Thanks! </p> </section> <div id="options"><button onclick="window.print();"><svg xmlns="http://www.w3.org/2000/svg" height="34" width="34" viewBox="0 0 48 48"><path d="M32.9 15.6V9H15.1v6.6h-3V6h23.8v9.6ZM7 18.6h34-28.9Zm29.95 4.75q.6 0 1.05-.45.45-.45.45-1.05 0-.6-.45-1.05-.45-.45-1.05-.45-.6 0-1.05.45-.45.45-.45 1.05 0 .6.45 1.05.45.45 1.05.45ZM32.9 39v-9.6H15.1V39Zm3 3H12.1v-8.8H4V20.9q0-2.25 1.525-3.775T9.3 15.6h29.4q2.25 0 3.775 1.525T44 20.9v12.3h-8.1ZM41 30.2v-9.3q0-1-.65-1.65-.65-.65-1.65-.65H9.3q-1 0-1.65.65Q7 19.9 7 20.9v9.3h5.1v-3.8h23.8v3.8Z" /></svg></button><span style="height: 30px; border-left: 2px solid #fff;"></span><a href="mailto:test@example.com"><svg xmlns="http://www.w3.org/2000/svg" height="34" width="34" viewBox="0 0 48 48"><path d="M7 40q-1.2 0-2.1-.9Q4 38.2 4 37V11q0-1.2.9-2.1Q5.8 8 7 8h34q1.2 0 2.1.9.9.9.9 2.1v26q0 1.2-.9 2.1-.9.9-2.1.9Zm17-15.1L7 13.75V37h34V13.75Zm0-3L40.8 11H7.25ZM7 13.75V11v26Z" /></svg></a></div></body></html>
-		`;
+<table><tr> <th>Date</th> <th>Project/Task</th> <th>Start</th> <th>Stop</th> <th>Hours</th> <th>Rate</th> <th>Total</th> </tr>${tableRowsContent}</table></section>${totalsContent}<section> <p> <b>Notice: </b>This invoice is dynamically generated from Toggl data and may be incomplete. The hour totals listed are rounded from the actual time tracked (in milliseconds). The total hours may be slightly off as a result. However, the amount due is calculated 
+off the seconds recorded. Timezones are in US EST (New York). Please contact ${me.fullname
+		} if you have any questions or require clarification on any item. Thanks! </p> </section> <div id="options"><button onclick="window.print();"><svg xmlns="http://www.w3.org/2000/svg" height="34" width="34" viewBox="0 0 48 48"><path d="M32.9 15.6V9H15.1v6.6h-3V6h23.8v9.6ZM7 18.6h34-28.9Zm29.95 4.75q.6 0 1.05-.45.45-.45.45-1.05 0-.6-.45-1.05-.45-.45-1.05-.45-.6 0-1.05.45-.45.45-.45 1.05 0 .6.45 1.05.45.45 1.05.45ZM32.9 39v-9.6H15.1V39Zm3 3H12.1v-8.8H4V20.9q0-2.25 1.525-3.775T9.3 15.6h29.4q2.25 0 3.775 1.525T44 20.9v12.3h-8.1ZM41 30.2v-9.3q0-1-.65-1.65-.65-.65-1.65-.65H9.3q-1 0-1.65.65Q7 19.9 7 20.9v9.3h5.1v-3.8h23.8v3.8Z" /></svg></button></div></body></html>
+		<script>window.onbeforeprint = function() { document.getElementById('options').style.display = 'none'; }; window.onafterprint = function() { document.getElementById('options').style.display = 'flex'; }; </script>`;
 	return html;
 }
